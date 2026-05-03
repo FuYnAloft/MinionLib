664221d4062a790ebe243d9febf3684a9fbaaac5
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-05-03 14:44:00 +0800

    feat: 组件支持清晰的Log

--- Component/Core/ComponentStateAttribute.cs
+++ Component/Core/ComponentStateAttribute.cs
@@ -3,10 +3,10 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 namespace MinionLib.Component.Core;
 
 [AttributeUsage(AttributeTargets.Property)]
-public sealed class ComponentStateAttribute : Attribute;
+public class ComponentStateAttribute : Attribute;
 
 [AttributeUsage(AttributeTargets.Property)]
-public sealed class ComponentStateAttribute<T>(params object[] parameters): Attribute
+public sealed class ComponentStateAttribute<T>(params object[] parameters): ComponentStateAttribute
     where T : DynamicVar
 {
     private readonly object[] _parameters = parameters;
